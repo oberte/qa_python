9d4287b72f48c356077d70895ea1374284798855
Marina Orangkhadivi <oberte@yandex.com>
2023-09-24 09:06:23 -0600

    new commit by Marina Orangkhadivi

--- tests.py
+++ tests.py
@@ -1,24 +1,70 @@
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Kniga_1')
+        assert 'Kniga_1' in collector.books_genre
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_2")
+        collector.set_book_genre("Kniga_2", "Детективы")
+        assert collector.get_book_genre("Kniga_2") == "Детективы"
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_3")
+        collector.set_book_genre("Kniga_3", "Комедии")
+        collector.add_new_book("Kniga_4")
+        collector.set_book_genre("Kniga_4", "Комедии")
+        assert collector.get_books_with_specific_genre("Комедии") == ["Kniga_3", "Kniga_4"]
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_5")
+        collector.set_book_genre("Kniga_5", "Мультфильмы")
+        collector.add_new_book("Kniga_6")
+        collector.set_book_genre("Kniga_6", "Ужасы")
+        assert collector.get_books_for_children() == ["Kniga_5"]
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_6")
+        collector.add_book_in_favorites("Kniga_6")
+        assert "Kniga_6" in collector.favorites
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_7")
+        collector.add_book_in_favorites("Kniga_7")
+        collector.delete_book_from_favorites("Kniga_7")
+        assert "Kniga_7" not in collector.favorites
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_8")
+        collector.add_new_book("Kniga_9")
+        collector.add_book_in_favorites("Kniga_8")
+        collector.add_book_in_favorites("Kniga_9")
+        assert collector.get_list_of_favorites_books() == ["Kniga_8", "Kniga_9"]
+
+    def test_limitation_on_book_name(self):
+        collector = BooksCollector()
+        book_name = "A" * 41
+        collector.add_new_book(book_name)
+        assert book_name not in collector.books_genre
+
+    def test_add_new_book_only_once(self):
+        collector = BooksCollector()
+        collector.add_new_book("Kniga_10")
+        collector.add_new_book("Kniga_10")
+        assert len(collector.books_genre) == 1
